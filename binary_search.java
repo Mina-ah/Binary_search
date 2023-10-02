009fe375314584db8bed2977688a4c8bb13828da
Mina-ah <114679512+Mina-ah@users.noreply.github.com>
2023-10-02 13:08:48 +0530

    Update binary_search.java

--- binary_search.java
+++ binary_search.java
@@ -6,7 +6,7 @@ public class Main
         int start=0;
         int end=arr.length-1;
         while(start<=end){
-            int mid=(start+end)/2;
+            int mid=start+(end-start)/2;
             if(arr[mid]==key){
                 return mid;
             }
